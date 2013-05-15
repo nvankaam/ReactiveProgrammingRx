27547660d46b236a923749458b1d14aa109d5be1
Filipe Meirinhos <filimeirinhos@gmail.com>
2013-05-15 17:34:04 +0200

    Example using logfile added.

---
+++ RxSplunkSolution/Data/Repo/FileOpener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repo
+{
+    public class FileOpener
+    {
+        public String[] LogFileLines{ get; set; }
+        public FileOpener()
+        {
+            LogFileLines = System.IO.File.ReadAllLines("Files/access_log.txt");
+        }
+    }
+}
